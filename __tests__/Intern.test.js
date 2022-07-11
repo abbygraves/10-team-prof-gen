51488040c5295d587175776829a2401f05e1f092
abbygraves <abbygraves14@gmail.com>
2022-07-11 08:00:22 -0500

    corrected intern test and completed manager test

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,19 +1,19 @@
 const Intern = require('../lib/Intern');
 
-const intern = new Intern("Michael Scott", "9622", "PrisonMike@dundermifflin.com", "TheOffice");
+const intern = new Intern("Kool-Aid Man", "543", "OhYeahhh@koolmail.com", "Wall Smashers University");
 
 // INTERN OBJECT TEST
 test("creates intern object", () => {
-  expect(intern.name).toBe("Michael Scott");
-  expect(intern.id).toBe("9622");
-  expect(intern.email).toBe("PrisonMike@dundermifflin.com");
-  expect(intern.school).toBe("TheOffice");
+  expect(intern.name).toBe("Kool-Aid Man");
+  expect(intern.id).toBe("543");
+  expect(intern.email).toBe("OhYeahhh@koolmail.com");
+  expect(intern.school).toBe("Wall Smashers University");
 });
 
 
 // TEST TO GET SCHOOL
 test("check if getSchool() returns school", () => {
-  expect(intern.getSchool()).toEqual("TheOffice");
+  expect(intern.getSchool()).toEqual("Wall Smashers University");
 });
 
 
@@ -23,5 +23,5 @@ test("check if getRole() returns role", () => {
 });
 
 
-
 // CODE COMPLETE
+
--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,3 +1,26 @@
-// TODO
+const Manager = require('../lib/Manager');
 
-// INCOMPLETE CODE
+const manager = new Manager("Michael Scott", "9622", "PrisonMike@dundermifflin.com", "TheOffice");
+
+// MANAGER OBJECT TEST
+test("creates manager object", () => {
+  expect(manager.name).toBe("Michael Scott");
+  expect(manager.id).toBe("9622");
+  expect(manager.email).toBe("PrisonMike@dundermifflin.com");
+  expect(manager.officeNumber).toBe("TheOffice");
+});
+
+
+// TEST TO GET OFFICE NUMBER
+test("check if getOfficeNumber() returns office number", () => {
+  expect(manager.getOfficeNumber()).toEqual("TheOffice");
+});
+
+
+// TEST TO GET ROLE
+test("check if getRole() returns role", () => {
+  expect(manager.getRole()).toEqual("Manager");
+});
+
+
+// CODE COMPLETE
